5ce55e041066e322c0b6839c6680b35899b2b3de
YORNSOKHLEN <yornsokhlen33@gmail.com>
2025-01-18 19:14:50 +0700

    src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDom from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
-import "./components/CSS/App.CSS";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
